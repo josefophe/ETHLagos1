8f6f58386b520c502871fed91fed3f38f2817e11
Joseph <josefophe@gmail.com>
2020-10-10 21:17:48 +0100

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+// import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import truffleContract from "truffle-contract";
+import IPFSInboxContract from "./contracts/IPFSInbox.json";
 import getWeb3 from "./getWeb3";
+import ipfs from './ipfs';
+
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { storageValue: 0,
+	  web3: null,
+	  accounts: null,
+	  contract: null,
+	  ipfsHash: null,
+	  formIPFS: "",
+	  formAddress: "",
+	  receivedIPFS: ""
+
+  	};
+
+	this.handleChangeAddress = this.handleChangeAddress.bind(this);
+	this.handleChangeIPFS = this.handleChangeIPFS.bind(this);
+	this.handleSend = this.handleSend.bind(this);
+	this.handleReceiveIPFS = this.handleReceiveIPFS.bind(this);
+
+  }
+
+  //state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -16,16 +38,14 @@ class App extends Component {
       const accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+      const Contract = truffleContract(IPFSInboxContract);
+      Contract.setProvider(web3.currentProvider);
+      const instance = await Contract.deployed();
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance });
+	this.setEventListeners();
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,17 +55,66 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+    // Turn the File submitted into a buffer
+  captureFile = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    const file = event.target.files[0];
+    let reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => this.convertToBuffer(reader);
+  };
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+  setEventListeners = () => {
+    this.state.contract.inboxResponse()
+      .on('data', result => {
+        this.setState({receivedIPFS:result.args[0]});
+      });
+  }
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+  // Helper function for turning a file into a buffer
+  convertToBuffer = async (reader) => {
+    const buffer =  await Buffer.from(reader.result);
+    this.setState({buffer});
+  };
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  handleChangeAddress = (event) => {
+    this.setState({formAddress: event.target.value});
+  }
+
+  handleChangeIPFS = (event) => {
+    this.setState({formIPFS: event.target.value});
+  }
+
+  handleSend = (event) => {
+    event.preventDefault();
+    const contract = this.state.contract;
+    const account = this.state.accounts[0];
+
+    document.getElementById('new-notification-form').reset();
+    this.setState({showNotification: true});
+    contract.sendIPFS(this.state.formAddress, this.state.formIPFS, {from: account})
+      .then(result => {
+        this.setState({formAddress: ""});
+        this.setState({formIPFS: ""});
+      })
+  }
+
+  handleReceiveIPFS = (event) => {
+    event.preventDefault();
+    const contract = this.state.contract;
+    const account = this.state.accounts[0];
+    contract.checkInbox({from: account});
+  }
+
+  // Function for sending the buffer to the
+  // ipfs node and show the ipfs hash on the UI
+  onIPFSSubmit =  async(event) => {
+    event.preventDefault();
+    await ipfs.add(this.state.buffer, (err, ipfsHash) => {
+      console.log(err, ipfsHash);
+      this.setState({ipfsHash:ipfsHash[0].hash});
+    })
   };
 
   render() {
@@ -54,20 +123,31 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h2>1. Add a file to IPFS here</h2>
+        <form id="ipfs-hash-form" className="scep-form" onSubmit={this.onIPFSSubmit}>
+          <input type="file" onChange={this.captureFile} />
+          <button type="submit">Send It</button>
+        </form>
+        <p>The IPFS hash is:{this.state.ipfsHash}</p>
+        <h2>2. Send Notification here.</h2>
+        <form id="new-notification-form" className="scep-form" onSubmit={this.handleSend}>
+          <label>
+            Receiver Address:
+            <input type="text" value={this.state.value} onChange={this.handleChangeAddress} />
+          </label>
+          <label>
+            IPFS Address:
+            <input type="text" value={this.state.value} onChange={this.handleChangeIPFS} />
+          </label>
+          <input type="submit" value="submit" />
+        </form>
+        <h2>3. Receive Notifications</h2>
+        <button onClick={this.handleReceiveIPFS}>Receive IPFS</button>
+        <p>{this.state.receivedIPFS}</p>
       </div>
     );
   }
 }
+
 
 export default App;
